6ab1ad080cbe8edb435ba9dd4c782e99a57882de
michellechristina <michelle@thisisbam.com>
2017-10-20 21:08:09 -0400

    more js

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,132 +20,121 @@
 
 
 
-var questionsAnwswered = 0;
+    var count = 60;
     var incorrectCounter = 0; 
     var correctCounter = 0;
     var unansweredCounter = 0;
-    var choices = []; // an array for guesses
-    var quiz =$('#quiz') // quiz div id class
+    // var quiz =$('#quiz') // quiz div id class
 
 
+    // let the game(s) begin
 
-var questions = [{
-        question: "what company logo uses the color brown?",
-        guesses: ["Nike", "Pizza Hut", "Netflix", "UPS"],
-        answer: 3,
-    },
-    {
-        question: "what company logo uses the color red?",
-        guesses: ["FedEx", "Skype", "Netflix", "Blogger"],
-        answer: 2,
-    },
+    $(document).ready(function(){
+        $("#quizDiv").hide(); // game starts w/ hidden divs
+        $("#resolutionDiv").hide();
 
-    {
-        question: "what company logo uses the color blue?",
-        guesses: ["Twitter", "Starbucks", "Hallmark", "BBC"],
-        answer: 0,
-    },
 
-    {
-        question: "what company logo uses the color purple?",
-        guesses: ["Target", "Yahoo", "Nikon", "Vimeo"],
-        answer: 1,
+        $("startButton").on("click", function() {  // this initializes the start page / button
+
+            $("#startDiv").hide();
+
+            $("#quizDiv").show();
+
+            starTimeclock();
+            return;
+        });
+
+
+        function timeclock(){ // initialize & displays counter 
+            count--; // the actual deincrementation of the timeclock
+
+            $('#timerValue').html(count + " seconds"); // sets counter value in html
+
+            $("#finishedButton").on("click", function(){ // if you hit finished button before time is up.
+                count = 0; 
+                return;
+            
+
+        });
+
+if(count == -1)
+{
+    timesUp() {
+        var one = $('input:radio[name="one"]:checked').val();
+        var two = $('input:radio[name="two"]:checked').val();
+        var three = $('input:radio[name="three"]:checked').val();
+        var four = $('input:radio[name="four"]:checked').val();
+      
+        if (one == undefined){
+            unansweredCounter++;
+        }
+        else if (one == "UPS") {
+            correctCounter++;
+        }
+        else {
+            incorrectCounter++;
+        }
+
+        if (two == undefined){
+            unansweredCounter++;
+        }
+        else if (one == "Netflix") {
+            correctCounter++;
+        }
+        else {
+            incorrectCounter++;
+        }
+
+        if (three == undefined){
+            unansweredCounter++;
+        }
+        else if (one == "Twitter") {
+            correctCounter++;
+        }
+        else {
+            incorrectCounter++;
+        }
+
+        if (four == undefined){
+            unansweredCounter++;
+        }
+        else if (one == "Yahoo") {
+            correctCounter++;
+        }
+        else {
+            incorrectCounter++;
+        }
+
+
 
     }
-];
+}
 
-timer();
-showQuiz();
-
-
-function timer(){
-    time = setTimeout(timesUp, 1000 * 60);
-    console.log("this is the timer");
     }
 
-    function stopClock() {
-        clearTimer(time);
-    }
 
-    function timesUp(){
-        $("#finished").show();
-        $("#replaybutton").show();
-        $("#quiz").hide();
-        $("#timeclock").hide();
-        stopClock();  
-    }
+// var questions = [{
+//         question: "what company logo uses the color brown?",
+//         guesses: ["Nike", "Pizza Hut", "Netflix", "UPS"],
+//         answer: 3,
+//     },
+//     {
+//         question: "what company logo uses the color red?",
+//         guesses: ["FedEx", "Skype", "Netflix", "Blogger"],
+//         answer: 2,
+//     },
 
-   var second;
-   var x;
+//     {
+//         question: "what company logo uses the color blue?",
+//         guesses: ["Twitter", "Starbucks", "Hallmark", "BBC"],
+//         answer: 0,
+//     },
 
-   function counter() {
-       seconds = document.getElementById("counter").innerHTML;
-       seconds = parseInt(second, 10);
+//     {
+//         question: "what company logo uses the color purple?",
+//         guesses: ["Target", "Yahoo", "Nikon", "Vimeo"],
+//         answer: 1,
 
-       if (seconds == 1) {
-           x = document.getElementById("counter");
-           return;
-       }
+//     }
+// ];
 
-       seconds--;
-       x = document.getElementById("counter");
-       x.innerHTML = seconds;
-       setTheTimer = setTimeout(counter, 1000);
-   }
-
-   counter();
-
-   function timerFunction() {
-       clearTimer(setTheTimer);
-   }
-
-   // play again button
-
-   $('#replaybutton').on("click", function() {
-    var incorrectCounter = 0; 
-    var correctCounter = 0;
-    var unansweredCounter = 0;
-    var choices = [];
-    $("#finished").hide();
-    $("#replaybutton").hide();
-    $("#quiz").show();
-    $("#timeclock").show();
-    timer();
-    document.getElementById("counter").innerHTML = 60;
-    counter();
-   });
-
-   // creates the quiz
-   function showQuiz() {
-       var quizInfo = $('div', {
-           id: 'question'
-       });
-
-       var question = $('<p>').append(questions.question);
-       quizInfo.append(question);
-
-       var radios = makeRadios();
-       quizInfo.append(radios);
-       return quizDiz;
-   };
-
-   function makeRadios(index) {
-       var radioChoices = $('<ul>');
-       var inputs = '';
-       for (var i = 0; i < questions[index].guesses.length; i++) {
-           var element = $('<li>');
-           inputs = '<input type="radio" name="guesses" value=' +i+ ' />';
-           inputs += questions[index].guesses[i]
-           item.append(inputs);
-           radios.append(item);
-       }
-       return radios;
-   }
-
-   // gets user guess & adds to array
-
-   function answerChoosen() {
-choices[questionsAnwswered] = +$('inputs[name="guesses"]:checked').val();
-   }
-
-   
